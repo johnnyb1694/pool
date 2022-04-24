e59c099a73cf048e5b33db5e747e553fa406d7f2
johnnyb1694 <johnnyb1694@gmail.com>
2022-04-24 17:19:51 +0100

    [Add]: (app.js). initial working version of application

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Received!</h1>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Application listening on port ${PORT}!`)
+  })
