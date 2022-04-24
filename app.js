1f04f95a93a4053fbbef08d4b77cd5a1cdac7c9b
johnnyb1694 <johnnyb1694@gmail.com>
2022-04-24 17:26:02 +0100

    [Add]: (*.html) added initial working versions of index and submit files!

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('<h1>Received!</h1>');
+app.get("/", (req, res) => {
+    res.send("<h1>Received!</h1>");
 });
 
 app.listen(PORT, () => {
-    console.log(`Application listening on port ${PORT}!`)
-  })
+    console.log(`Application listening on port ${PORT}!`);
+});
