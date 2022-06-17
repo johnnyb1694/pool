bc4450ffdcee2be32921da82cc3d4e7aefda9745
johnnyb1694 <johnnyb1694@gmail.com>
2022-06-17 19:08:09 +0100

    [Add]: (*.html) added initial templates to the repo

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Hello, world!"
+    return render_template('welcome.html')
+
+@app.route('/submit-transaction', methods = ['GET', 'POST'])
+def submit_transaction():
+    if request.method == 'POST':
+        print('Posting transaction...')
+        desc = request.form['desc']
+        print(desc)
+    else:
+        print('Getting transaction page...')
+    return render_template('transaction.html')
